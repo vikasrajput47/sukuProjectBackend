5170a95b795e8fac30077d67857376e382167655
Vikas Rajput <95566107+vikasrajput47@users.noreply.github.com>
2023-12-26 20:24:40 +0530

    Update database.js

--- data/database.js
+++ data/database.js
@@ -3,8 +3,13 @@ import mongoose from "mongoose";
 export const connectDB = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
       dbName: "suku",
     })
     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
-    .catch((e) => console.log(e));
+    .catch((error) => {
+      console.error('MongoDB connection error:', error);
+      process.exit(1); // Exit the application on connection error
+    });
 };
