3add142457ab774aab72bd10099849b2e7c17108
vikasrajput47 <vikasrajpoot479@gmail.com>
2023-12-26 19:57:25 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import userRouter from "./routes/user.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
+
+export const app = express();
+
+config({
+  path: "./data/config.env",
+});
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// Using routes
+app.use("/user", userRouter);
+
+
+app.get("/", (req, res) => {
+    res.send("Nice working");
+});
+
+
+// Using Error Middleware
+app.use(errorMiddleware);
---
+++ routes/user.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getMyProfile, login, logout, register } from "../controllers/user.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+
+router.get("/logout", logout);
+
+router.get("/me", isAuthenticated, getMyProfile);
+
+export default router;
